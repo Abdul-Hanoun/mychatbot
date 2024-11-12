cd0008376aa027ea45e73e462c5df5f325cb1799
Abdul-Hanoun <abdulhanoun@icloud.com>
2024-11-12 21:12:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,19 +7,10 @@ st.set_page_config(page_title="🤗💬 HugChat")
 
 # Hugging Face Credentials
 with st.sidebar:
-    st.title('🤗💬 'ok')
-    if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
-        st.success('HuggingFace Login credentials already provided!', icon='✅')
-        hf_email = st.secrets['EMAIL']
-        hf_pass = st.secrets['PASS']
-    else:
-        hf_email = st.text_input('Enter E-mail:', type='password')
-        hf_pass = st.text_input('Enter password:', type='password')
-        if not (hf_email and hf_pass):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-an-llm-powered-chatbot-with-streamlit/)!')
+    st.title('Chat Bot')
+ 
+     st.success('Proceed to entering your prompt message!', icon='👉')
+  
     
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
